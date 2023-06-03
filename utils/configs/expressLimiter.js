9dc50fe8e6af8fe08d5c5a073fea8d533755559c
Denis Shiryaev/ <arokmeister@yandex.ru/>
2023-06-03 16:21:26 +0300

    fix: изменение лимитера

--- utils/configs/expressLimiter.js
+++ utils/configs/expressLimiter.js
@@ -1,8 +1,8 @@
 const expressRateLimit = require('express-rate-limit');
 
 const limiter = expressRateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 2 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 });
 
